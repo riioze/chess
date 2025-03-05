82fc950b10b5cb32865a8578911a16fff53d149b
Nino Keravel <61903425+riioze@users.noreply.github.com>
2025-03-05 15:00:19 +0100

    remove SDL for now

--- Code/source/main.cpp
+++ Code/source/main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 
-#include<SDL3/SDL.h>
-
 #include "piece/piece.hpp"
 #include "move/move.hpp"
 #include "board/board.hpp"
 
 int main() {
     
-    SDL_acos(0);
-
     return 0;
 }
