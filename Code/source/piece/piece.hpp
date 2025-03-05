16c47c9b79093d73e6e36b827f84880672b5ce71
Nino Keravel <61903425+riioze@users.noreply.github.com>
2025-03-05 15:18:37 +0100

    delete all and make clean bases

--- Code/source/piece/piece.hpp
+++ Code/source/piece/piece.hpp
@@ -2,20 +2,22 @@
 // Created by Nino Keravel on 19/02/2025.
 //
 
-
 #include "../move/move.hpp"
 #include "../board/board.hpp"
+
+#include <vector>
 #ifndef PIECE_H
 #define PIECE_H
-
 
 class Piece {
     public:
     char symbol;
     bool isWhite;
-    Piece(char symbol, bool isWhite);
+    Board board;
+    int x, y;
+    Piece(bool isWhite, Board board, int x, int y) : isWhite(isWhite), board(board), x(x), y(y) {}
     virtual ~Piece() = default;
-    virtual bool isValidMove(const Move& move, const Board& board) = 0;
+    virtual std::vector<Move> getValidMoves() = 0;
 };
 
 #endif //PIECE_H
